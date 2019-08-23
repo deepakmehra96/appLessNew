939325aa37a29200443d336d662fd3cf0fe6d85e
deepak <deepakmehra149@gmail.com>
2019-08-23 20:02:24 +0530

    build made

---
+++ build/precache-manifest.971e5512f6c8f378bc81ae079efbdce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729c22c4018ba92e3fd4e536285ac58d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b86698edece14c5a4d",
+    "url": "/static/css/main.8313e72e.chunk.css"
+  },
+  {
+    "revision": "414e538c999e5fac5cc9",
+    "url": "/static/js/2.9b8d2e5a.chunk.js"
+  },
+  {
+    "revision": "86b86698edece14c5a4d",
+    "url": "/static/js/main.fa8a5f1f.chunk.js"
+  },
+  {
+    "revision": "2d59d31398e6378296a2",
+    "url": "/static/js/runtime~main.47ff7184.js"
+  },
+  {
+    "revision": "a2e9b7c1a25043babe15db454814857c",
+    "url": "/static/media/tic-tac-toe.a2e9b7c1.png"
+  }
+]);
