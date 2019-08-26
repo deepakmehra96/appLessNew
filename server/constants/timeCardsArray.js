bb663359562c77a6de0deff885262b04a61d3003
deepak <deepakmehra149@gmail.com>
2019-08-26 18:42:53 +0530

    minnor issue resolved

--- server/constants/timeCardsArray.js
+++ server/constants/timeCardsArray.js
@@ -1,5 +1,5 @@
 let timeCardsArray = [
-    { image: './card.ico', number: 'Card 1', title: '7 Seconds' },
+    { image: '/card.ico', number: 'Card 1', title: '7 Seconds' },
     { image: '/card.ico', number: 'Card 2', title: '27 Seconds' },
     { image: '/card.ico', number: 'Card 3', title: '17 Seconds' },
     { image: '/card.ico', number: 'Card 4', title: '47 Seconds' },
